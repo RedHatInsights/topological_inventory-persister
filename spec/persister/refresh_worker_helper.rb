0f2b134ccbb759a56afc02d979aeb78a425d2b41
Adam Grare <agrare@redhat.com>
2019-04-05 08:14:02 -0400

    Fix spec failures on ReceivedMessage

--- spec/persister/refresh_worker_helper.rb
+++ spec/persister/refresh_worker_helper.rb
@@ -2,7 +2,7 @@ module TestInventory
   module RefreshWorkerHelper
     def refresh(client, path)
       inventory = JSON.parse(File.read(test_inventory_dir.join(*path)))
-      messages = [ManageIQ::Messaging::ReceivedMessage.new(nil, nil, inventory, nil)]
+      messages = [ManageIQ::Messaging::ReceivedMessage.new(nil, nil, inventory, nil, client)]
 
       allow(client).to receive(:subscribe_messages).and_yield(messages)
 
